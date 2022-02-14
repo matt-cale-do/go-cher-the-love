5e7e9294abe32a889959eba984d595099c834eee
mcale <mcale@digitalocean.com>
2022-02-14 12:17:28 -0600

    feat: use gorilla mux over http lib

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-cher-the-love
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
+const defaultPort = "80"
+
 func sing(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Do you believe in life after love?\n")
+	fmt.Fprintf(w, "Do you believe in life after love?\n")
 }
 
 func walking(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "In Memphis\n")
+	fmt.Fprintf(w, "In Memphis\n")
 }
 
 func main() {
-    http.HandleFunc("/", sing)
-    http.HandleFunc("/sing", sing)
-    http.HandleFunc("/walking", walking)
-    http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", sing)
+	r.HandleFunc("/sing", sing)
+	r.HandleFunc("/walking", walking)
+	fmt.Printf("==> Server listening at :%s 🚀\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
